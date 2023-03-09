da4771518fdd9d50bded62bf9a9609735fc54b8f
Build Er <builder@macmini6.local>
2023-03-09 10:54:49 -0300

    Package version: 1.0.0-beta+20230309134517

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXUCRelativeTimerWrapper",
 				dependencies: [
 					"GXUCRelativeTimer",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.watchOS, .tvOS, .iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXUCRelativeTimer",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXUCRelativeTimer-1.0.0-beta+20230309100338.xcframework.zip",
-			checksum: "551c803d594687489e823ec24530fb44709d4f6e99a306d11404886d3ec2e10b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXUCRelativeTimer-1.0.0-beta+20230309134517.xcframework.zip",
+			checksum: "4165e611e0e8c90d720d97f88b181e65429d70ed0e3d48019a934f230872d7c0"
 		)
 	]
 )
