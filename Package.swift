78b4b6987b5c5033f46c9685e11d792c732188c5
genexusbot <builder@genexus.com>
2023-07-14 16:31:12 -0300

    Package version: 1.0.0-preview.20230714192150

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXUCRelativeTimer",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXUCRelativeTimer",
+			targets: ["GXUCRelativeTimerWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-preview.20230714192150")
+	],
+	targets: [
+		.target(name: "GXUCRelativeTimerWrapper",
+				dependencies: [
+					"GXUCRelativeTimer",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS, .watchOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXUCRelativeTimer",
+			url: "https://pkgs.genexus.dev/iOS/preview/GXUCRelativeTimer-1.0.0-preview.20230714192150.xcframework.zip",
+			checksum: "cf99d47d57a0fe5ca1a0e6f78a58ea7b0c1031b3fd54dcf9d049fdde20305f50"
+		)
+	]
+)
